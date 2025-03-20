f2a2b608e3c04aef1a24aec68f6658435eff0f97
Joshua-M <137477063+Joshua-M@users.noreply.github.com>
2025-03-20 09:16:08 +0200

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -29,13 +29,12 @@ bg_image = st.file_uploader("Upload Background Image (PNG)", type=["png"])
 if bg_image is not None:
     set_background(bg_image)
 
-st.title(" :bar_chart: MIH - Exploratory Data Analysis")
+st.title(" :bar_chart: Exploratory Data Analysis")
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
 
 # File Uploader for Data
 df = None
-fl = 
-else:
+fl = st.file_uploader(":file_folder: Upload a file", type=["csv", "txt", "xlsx", "xls"])else:
     sample_file = "Sample - Superstore.xls"
     if os.path.exists(sample_file):
         df = pd.read_excel(sample_file, engine='xlrd')  # Specify engine for .xls files
