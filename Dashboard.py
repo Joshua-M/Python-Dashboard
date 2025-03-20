a0d308076de758e1f0ce0889f1feb46805c1fd11
Joshua-M <137477063+Joshua-M@users.noreply.github.com>
2025-03-20 09:22:52 +0200

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -2,14 +2,6 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 import os
-import warnings
-import base64
-
-warnings.filterwarnings('ignore')
-
-# Set the page config
-st.set_page_config(page_title="Dashboard", page_icon='bar_chart', layout='wide')
-
 
 st.title(" :bar_chart: Exploratory Data Analysis")
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
